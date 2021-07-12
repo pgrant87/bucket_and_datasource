134375f27083304a0bbee33d20284dde22381a2c
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2021-07-12 18:05:25 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 class testmodule {
 
-dsc_virtualmemory {'c:':
-dsc_initialsize => 1024,
-dsc_maximumsize => 8192,
-dsc_type => 'CustomSize',
-dsc_drive => 'c:',
-validation_mode => 'resource',
-}
+  dsc_virtualmemory {'c:':
+    validation_mode => 'resource',
+    dsc_initialsize => 1024,
+    dsc_maximumsize => 8192,
+    dsc_type => 'CustomSize',
+    dsc_drive => 'c:',
+  }
 
 }
