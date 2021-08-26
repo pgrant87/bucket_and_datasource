21d5a2f6adb22c50030b007f57756cb932436656
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2021-08-26 11:54:26 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,5 @@
 class testmodule {
 
-  windowsfeature { 'ad_powershell':
-    ensure => present,
-    name => 'RSAT-AD-PowerShell',
-  }
-
-  dsc_computer { 'join_domain':
-    dsc_name => 'localhost',
-    dsc_domainname => 'test.com',
-    dsc_joinou => 'test',
-    dsc_credential => {
-      'user' => 'testuser',
-      'password' => Sensitive('testpassword'),
-    },
-  }
+  notify { "Fact uptime: ${facts['uptime']}":}
 
 }
