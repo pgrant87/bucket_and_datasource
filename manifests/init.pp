955b5ef5e484466ee38920caffebc0c6e312e07c
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2022-04-29 18:30:22 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,30 @@
-class testmodule{
+class bucket_and_datasource(
+  Sensitive[String] $token,
+){
 
-user { 'paddy':
-  ensure     => present,
-  uid        => '1000',
-  gid        => '1000',
-  shell      => '/bin/bash',
-  home       => '/home/mitchell'
-}
+  influxdb_bucket {'customer_bucket':
+    ensure => present,
+    org    => 'puppetlabs',
+    token  => $token,
+  }
 
+  grafana_datasource {'customer_datasource':
+    grafana_user     => 'admin',
+    grafana_password => 'admin',
+    grafana_url      => "http://${facts['fqdn']}:3000",
+    type             => 'influxdb',
+    # This must match the namevar of the influxdb_bucket resource
+    database         => 'customer_bucket',
+    url              => "https://${facts['fqdn']}:8086",
+    access_mode      => 'proxy',
+    is_default       => false,
+    json_data        => {
+      httpHeaderName1 => 'Authorization',
+      httpMode        => 'GET',
+      tlsSkipVerify   => true
+    },
+    secure_json_data => {
+      httpHeaderValue1 => 'Token 6vtOFbN_3IEM9ts_AuBYCgP-DSjVEacxJIxnggTU-SQLskR8OzW3vY76Tvsf-rzBJfgaurKhPZiH6FstgNahaA==',
+    },
+  }
 }
